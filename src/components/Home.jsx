e91bd86504db67c9ce7fa2b728330b4dc6cc92e0
Muhammad Danish Azeem <88603558+mda1458@users.noreply.github.com>
2023-07-23 09:46:16 +0500

    updated home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -38,9 +38,6 @@ const Home = () => {
       console.error(error);
     }
   };
-  const searchAuthor = (e) => {
-    e.preventDefault();
-  };
   return (
     <div className="relative max-w-5xl mx-4 md:mx-auto pt-16">
       <h1 className="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center">
@@ -64,27 +61,6 @@ const Home = () => {
           name="search"
           required
           placeholder="Quick Search Books ..."
-        />
-        <button
-          type="submit"
-          className="inline-flex text-3xl items-center px-4 py-3 border border-transparent font-medium rounded-3xl shadow-sm text-black transition-all duration-500 hover:bg-[var(--primary-color)] hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[var(--primary-color)] "
-        >
-          <BiSearchAlt />
-        </button>
-      </form>
-      <div className="flex items-center justify-center my-6">
-        <p className="text-slate-300 text-lg">OR</p>
-      </div>
-      <form
-        onSubmit={searchAuthor}
-        className="flex justify-center space-x-6 text-sm"
-      >
-        <input
-          type="text"
-          className="px-2 py-1 transition-all duration-500 hover:ring-2 hover:ring-[var(--primary-color)] focus:ring-2 focus:ring-[var(--primary-color)] focus:ring-offset-4 outline-none rounded-3xl shadow-xl w-64"
-          name="search"
-          required
-          placeholder="Quick Search Authors ..."
         />
         <button
           type="submit"
